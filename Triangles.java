d298323709e0c498b315d76cec5d63427026e591
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-06-28 00:37:16 +0200

    Got the math right, also random colors

--- Triangles.java
+++ Triangles.java
@@ -1,17 +1,12 @@
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Panel;
-import java.awt.Toolkit;
-
-import javax.swing.JComponent;
+import java.util.Random;
 
 public class Triangles extends Panel {
 		
 		int width = 500;
 		int height = 500;
-		int side = (int) ((height/(Math.sqrt(3))) * 2);
 		int[] a = { width/2, 0, width }; // Top, Left, Right
 		int[] b = { 0, height, height };
 		
@@ -23,45 +18,37 @@ public class Triangles extends Panel {
 			if(x[0] != 1) {
 			g.setColor(Color.GRAY);
 			g.fillPolygon(x, y, 3);
-			int[] xl = {x[0]/2, x[1], x[2]/2};
-			drawTriangle(g, a, b);
+			drawTriangle(g, a, b, 5);
 			}
 		}
 		
-		public void drawTriangle(Graphics g, int[] x, int[] y) {
+		public void drawTriangle(Graphics gr, int[] x, int[] y, int level) {
+			int r = new Random().nextInt(250);
+			int g = new Random().nextInt(250);
+			int b = new Random().nextInt(250);
 			
-			//Dreieck links 
+			//Triangle top
+			gr.setColor(new Color(r,g,b));//Points Top, Left, Right
+			int[] xc = {x[0], (x[0] + x[1]) / 2, (x[0] + x[2]) / 2};
+			int[] yc = {y[0], (y[0] + y[1]) / 2, (y[0] + y[2]) / 2};
+			gr.fillPolygon(xc, yc, 3);
 			
-			g.setColor(Color.BLUE);
-			int[] xa = {x[0] - x[0]/2, x[1] + x[1], x[2] - x[2]/2};
-			int[] ya = {y[0] + y[1]/2, y[1],y[1]};
+			//Triangle left
+			gr.setColor(new Color(r,g,b));//Points Top, Left, Right
+			int[] xa = {(x[0] + x[1]) / 2, x[1], (x[1] + x[2]) / 2};
+			int[] ya = {(y[0] + y[1]) / 2, y[1], (y[1] + y[2]) / 2};
+			gr.fillPolygon(xa, ya, 3);
 			
-			System.out.println(x[0] + "");
-			System.out.println(xa[0] + "");
+			//Triangle right
+			gr.setColor(new Color(r,g,b));//Points Top, Left, Right
+			int[] xb = {(x[0] + x[2]) / 2, (x[1] + x[2]) / 2, x[2]};
+			int[] yb = {(y[0] + y[2]) / 2, (y[1] + y[2]) / 2, y[2]};
+			gr.fillPolygon(xb, yb, 3);
 			
-			g.fillPolygon(xa, ya, 3);
-			
-			//Dreieck rechts
-			
-			g.setColor(Color.GREEN);
-			int[] xb = {x[0] + x[0]/2, x[1] + x[0], x[2]};
-			int[] yb = {y[0] + y[1]/2, y[1], y[1]};
-			g.fillPolygon(xb, yb, 3);
-		
-			//Dreieck oben
-			
-			g.setColor(Color.RED);
-			int[] xc = {x[0], x[1] + x[0]/2, x[0] + x[0]/2};
-			int[] yc = {y[0], y[1] - y[1]/2, y[2] - y[1]/2};
-			g.fillPolygon(xc, yc, 3);
-			
-
-			drawTriangle(g, xa, ya);
-			drawTriangle(g, xb, yb);
-			drawTriangle(g, xc, yc);
-		}
-		
-		public void drawRecursion(Graphics g, int[] x, int[] y) {
-			
+			if (level > 0) {
+			drawTriangle(gr, xa, ya, level-1);
+			drawTriangle(gr, xb, yb, level-1);
+			drawTriangle(gr, xc, yc, level-1);
+			}
 		}
 	}
