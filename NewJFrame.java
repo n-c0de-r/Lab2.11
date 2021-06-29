376c895d372655740c4d56b1529bb4facaac6a21
sonke-s <82772330+sonke-s@users.noreply.github.com>
2021-06-29 17:40:27 +0200

    added eventListener for Resize

--- NewJFrame.java
+++ NewJFrame.java
@@ -1,5 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.awt.event.WindowStateListener;
 
 /**
  * This class creates a new window in the size of the current screen
@@ -8,37 +13,60 @@ import java.awt.*;
  * @author n-c0de-r
  * @version 28.06.2021
  */
-public class NewJFrame {
+public class NewJFrame extends JFrame implements ComponentListener{
 
-	private int screenHeight;
-	private int screenWidth;
+	
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();	
+	
+	private int height;
+	private int width;
 
-	static JFrame jf = new JFrame();
-
+	private JFrame jf = new JFrame();
+	
+	private Graphics g;
+	
 	public static void main(String[] args) {
 
 		NewJFrame j = new NewJFrame();
 		j.setup();
-		j.triangles();
 	}
 
 	/**
 	 * This method creates the Window
 	 */
-	public void setup() {
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		screenHeight = screenSize.height;
-		screenWidth = screenSize.width;
-
+	private void setup() {
+		
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jf.setSize(screenWidth, screenHeight);
+		jf.addComponentListener(this);
+		jf.setSize(screenSize.width, screenSize.height);
+		jf.setVisible(true);
+		g = jf.getGraphics();
 	}
 
-	/**
-	 * This method prints Sierpinski Triangles recursively
-	 */
-	public void triangles() {
-		jf.add(new Triangles());
-		jf.setVisible(true);
+	@Override
+	public void componentResized(ComponentEvent e) {
+		// TODO Auto-generated method stub
+		Dimension newSize = jf.getSize();
+		width = newSize.width;
+		height = newSize.height;
+		jf.add(new Triangles(height, g));
+	}
+
+	@Override
+	public void componentMoved(ComponentEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void componentShown(ComponentEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void componentHidden(ComponentEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
