01b1a8885934633db607e6f32702cdf736e1a4a4
sonke-s <82772330+sonke-s@users.noreply.github.com>
2021-06-29 17:41:52 +0200

    Triangle Colors, middle Triangle recursive
    Levels throw exception

--- Triangles.java
+++ Triangles.java
@@ -4,27 +4,34 @@ import java.awt.Panel;
 import java.util.Random;
 
 public class Triangles extends Panel {
+	
+		private int width;
+		private int height;
 		
-		int width = 500;
-		int height = 500;
-		int[] a = { width/2, 0, width }; // Top, Left, Right
-		int[] b = { 0, height, height };
+		private Color[] colors = {Color.BLUE, Color.CYAN, Color.YELLOW, Color.MAGENTA, Color.GREEN, Color.RED, Color.PINK, Color.ORANGE};
 		
-		public void paint(Graphics g) {	
-			draw(g, a, b);
+		private int[] a = {0,0,0}; // Top, Left, Right
+		private int[] b = {0,0,0};
+		
+		public Triangles(int h, Graphics g) {
+			
+			width = h;
+			height = h;
+			
+			a[0] = width/2;
+			a[1] = 0;
+			a[2] = width;
+			
+			b[0] = 0;
+			b[1] = height;
+			b[2] = height;
+			
+			this.paint(g);
 		}
 		
-		/**
-		 * This draws the main big triangle and calls recursion.
-		 * 
-		 * @param g Graphics object.
-		 * @param x Array of points x coordinates.
-		 * @param y Array of points y coordinates.
-		 */
-		public void draw(Graphics g, int[] x, int[] y) {
-			g.setColor(Color.GRAY);
-			g.fillPolygon(x, y, 3);
-			drawTriangles(g, a, b, 5);
+		public void paint(Graphics g) {
+			//System.out.println(width);
+			drawTriangles(g, a, b, 6);
 		}
 		
 		/**
@@ -36,25 +43,31 @@ public class Triangles extends Panel {
 		 * @param level	current level of recursion
 		 */
 		public void drawTriangles(Graphics gr, int[] x, int[] y, int level) {
-			//Pick random colors
-			int r = new Random().nextInt(250);
-			int g = new Random().nextInt(250);
-			int b = new Random().nextInt(250);
+
+			gr.setColor(colors[level]);
+			System.out.print(level);
+			//Triangle big
+			//Points Top, Left, Right
+			int[] xd = {x[0], x[1], x[2]};
+			int[] yd = {y[0], y[1], y[2]};
+			gr.fillPolygon(xd, yd, 3);
+			
+			//Math derived from here https://courses.cs.duke.edu//fall01/cps001/labs/lab7.html
 			
 			//Triangle top
-			gr.setColor(new Color(r,g,b));//Points Top, Left, Right
+			//Points Top, Left, Right
 			int[] xc = {x[0], (x[0] + x[1]) / 2, (x[0] + x[2]) / 2};
 			int[] yc = {y[0], (y[0] + y[1]) / 2, (y[0] + y[2]) / 2};
 			gr.fillPolygon(xc, yc, 3);
 			
 			//Triangle left
-			gr.setColor(new Color(r,g,b));//Points Top, Left, Right
+			//Points Top, Left, Right
 			int[] xa = {(x[0] + x[1]) / 2, x[1], (x[1] + x[2]) / 2};
 			int[] ya = {(y[0] + y[1]) / 2, y[1], (y[1] + y[2]) / 2};
 			gr.fillPolygon(xa, ya, 3);
 			
 			//Triangle right
-			gr.setColor(new Color(r,g,b));//Points Top, Left, Right
+			//Points Top, Left, Right
 			int[] xb = {(x[0] + x[2]) / 2, (x[1] + x[2]) / 2, x[2]};
 			int[] yb = {(y[0] + y[2]) / 2, (y[1] + y[2]) / 2, y[2]};
 			gr.fillPolygon(xb, yb, 3);
