eaf5f74a66d97113de9290aee9275da185d27826
sonke-s <82772330+sonke-s@users.noreply.github.com>
2021-06-30 21:39:12 +0200

    Final NewJFrame class with getPane()

--- NewJFrame.java
+++ NewJFrame.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 
 /**
  * This class creates a new window in the size of the current screen
@@ -10,17 +8,9 @@ import java.awt.event.ComponentListener;
  * @author n-c0de-r
  * @version 28.06.2021
  */
-public class NewJFrame extends JFrame implements ComponentListener{
+public class NewJFrame extends JFrame {
 
-	
-	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();	
-	
-	private int height;
-	private int width;
-
-	private JFrame jf = new JFrame();
-	
-	private Graphics g;
+	private static JFrame jf;
 	
 	public static void main(String[] args) {
 
@@ -32,38 +22,22 @@ public class NewJFrame extends JFrame implements ComponentListener{
 	 * This method creates the Window
 	 */
 	private void setup() {
+		jf = new JFrame("Sierpinski's Recursive Triangles by S�nke & Nermin");
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jf.addComponentListener(this);
 		jf.setSize(screenSize.width, screenSize.height);
+		jf.add(new Triangles());
 		jf.setVisible(true);
-		g = jf.getGraphics();
-		jf.add(new Triangles(jf.getContentPane().getHeight(), g));
 	}
-
-	@Override
-	public void componentResized(ComponentEvent e) {
-		// TODO Auto-generated method stub
-		Dimension newSize = jf.getSize();
-		width = newSize.width;
-		height = newSize.height;
-	}
-
-	@Override
-	public void componentMoved(ComponentEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void componentShown(ComponentEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void componentHidden(ComponentEvent e) {
-		// TODO Auto-generated method stub
-		
+	
+	/**
+	 * getter method to return the current content pane,
+	 * as the Frame itself is private and can't be accessed.
+	 * 
+	 * @return Container within the Frame.
+	 */
+	public static Container getPane() {
+		return jf.getContentPane();
 	}
 }
